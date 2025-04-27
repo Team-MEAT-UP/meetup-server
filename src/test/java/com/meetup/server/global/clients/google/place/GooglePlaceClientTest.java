c722a851a1d38f34186d8a0aa6519249220742c8
anxi01 <tjdals2893@naver.com>
2025-04-27 18:34:26 +0900

    TEST: (#13) Google 사진 조회 API를 테스트한다

---
+++ src/test/java/com/meetup/server/global/clients/google/place/GooglePlaceClientTest.java
@@ -0,0 +1,47 @@
+package com.meetup.server.global.clients.google.place;
+
+import com.meetup.server.global.clients.google.place.photo.GooglePhotoClient;
+import com.meetup.server.global.clients.google.place.photo.GooglePhotoRequest;
+import com.meetup.server.global.clients.google.place.photo.GooglePhotoResponse;
+import com.meetup.server.global.clients.google.place.search.GoogleSearchTextClient;
+import com.meetup.server.global.clients.google.place.search.GoogleSearchTextRequest;
+import com.meetup.server.global.clients.google.place.search.GoogleSearchTextResponse;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class GooglePlaceClientTest {
+
+    @Autowired
+    private GoogleSearchTextClient googleSearchTextClient;
+
+    @Autowired
+    private GooglePhotoClient googlePhotoClient;
+
+    @Test
+    void 구글_텍스트_검색_요청에_성공하고_장소_사진_조회에_성공한다() {
+        GoogleSearchTextRequest request = GoogleSearchTextRequest.from("스타벅스 방배점");
+        GoogleSearchTextResponse response = googleSearchTextClient.sendRequest(request);
+
+        assertNotNull(response);
+        assertNotNull(response.places()[0].id());
+
+        String name = response.places()[0].photos()[0].name();
+        assertNotNull(name);
+
+        GooglePhotoRequest photoRequest = GooglePhotoRequest.builder()
+                .name(name)
+                .maxHeightPx(1600)
+                .maxWidthPx(1600)
+                .build();
+
+        GooglePhotoResponse photoResponse = googlePhotoClient.sendRequest(photoRequest);
+
+        assertNotNull(photoResponse);
+        assertNotNull(photoResponse.photoUri());
+    }
+}
+
