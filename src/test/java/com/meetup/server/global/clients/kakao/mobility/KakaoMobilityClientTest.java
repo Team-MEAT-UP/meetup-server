7c1ba88d70811463c9ce91a40cae39db4d1c9eb3
anxi01 <tjdals2893@naver.com>
2025-04-27 18:31:03 +0900

    TEST: (#13) 카카오 자동차 길찾기 API를 테스트한다

---
+++ src/test/java/com/meetup/server/global/clients/kakao/mobility/KakaoMobilityClientTest.java
@@ -0,0 +1,30 @@
+package com.meetup.server.global.clients.kakao.mobility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class KakaoMobilityClientTest {
+
+    @Autowired
+    private KakaoMobilityClient kakaoMobilityClient;
+
+    @Test
+    void 카카오_자동차_길찾기_조회에_성공한다() {
+        // given
+        KakaoMobilityRequest request = KakaoMobilityRequest.builder()
+                .origin("127.10764191124568,37.402464820205246")
+                .destination("127.11056336672839,37.39419693653072")
+                .build();
+
+        // when
+        KakaoMobilityResponse response = kakaoMobilityClient.sendRequest(request);
+
+        // then
+        assertNotNull(response);
+        assertNotNull(response.transId());
+    }
+}
