efce118b575a20ebb452c891ad74aba9257e5bf4
syjdjr <syjdjr@naver.com>
2025-04-30 18:08:17 +0900

    FEAT: (#15) 트랜잭션(read-only) 적용한다

--- src/main/java/com/meetup/server/user/application/UserService.java
+++ src/main/java/com/meetup/server/user/application/UserService.java
@@ -8,10 +8,12 @@ import com.meetup.server.user.persistence.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class UserService {
 
     private final UserRepository userRepository;
