d7aa1ce39b6dce06b2d8cc5d7b8096e859b2e589
anxi01 <tjdals2893@naver.com>
2025-05-04 23:37:36 +0900

    FEAT: (#30) csv를 기반으로 Subway 엔티티에 필드를 추가한다

--- src/main/java/com/meetup/server/subway/domain/Subway.java
+++ src/main/java/com/meetup/server/subway/domain/Subway.java
@@ -1,6 +1,9 @@
 package com.meetup.server.subway.domain;
 
+import com.meetup.server.global.domain.BaseEntity;
+import com.meetup.server.startpoint.domain.type.Location;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -8,10 +11,30 @@ import lombok.NoArgsConstructor;
 @Table(name = "subway")
 @NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
 @Getter
-public class Subway {
+public class Subway extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "subway_id")
-    private Long subwayId;
+    private Integer subwayId;
+
+    @Column(name = "subway_name", nullable = false)
+    private String name;
+
+    @Column(name = "subway_code", nullable = false)
+    private Integer code;
+
+    @Column(name = "subway_line", nullable = false)
+    private Integer line;
+
+    @Embedded
+    private Location location;
+
+    @Builder
+    public Subway(String name, Integer code, Integer line, Location location) {
+        this.name = name;
+        this.code = code;
+        this.line = line;
+        this.location = location;
+    }
 }
