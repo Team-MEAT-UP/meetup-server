18fa1da0938e8f2325599cead73bbbc10a86d93a
anxi01 <tjdals2893@naver.com>
2025-05-04 23:39:07 +0900

    FEAT: (#30) SubwayConnection(지하철 노선) 엔티티를 추가한다

---
+++ src/main/java/com/meetup/server/subway/domain/SubwayConnection.java
@@ -0,0 +1,41 @@
+package com.meetup.server.subway.domain;
+
+import com.meetup.server.global.domain.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "subway_connection")
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+@Getter
+public class SubwayConnection extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "subway_connection_id")
+    private Integer subwayConnectionId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "from_subway_id", nullable = false)
+    private Subway fromSubway;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "to_subway_id", nullable = false)
+    private Subway toSubway;
+
+    @Column(name = "subway_line", nullable = false)
+    private Integer line;
+
+    @Column(name = "section_time_sec", nullable = false)
+    private Integer sectionTimeSec;
+
+    @Builder
+    public SubwayConnection(Subway fromSubway, Subway toSubway, Integer line, Integer sectionTimeSec) {
+        this.fromSubway = fromSubway;
+        this.toSubway = toSubway;
+        this.line = line;
+        this.sectionTimeSec = sectionTimeSec;
+    }
+}
