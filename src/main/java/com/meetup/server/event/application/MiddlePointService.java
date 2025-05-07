f8fa95aba7a17e28bdecc2ea0e1b3bb72a33ee6c
syjdjr <syjdjr@naver.com>
2025-05-07 15:57:38 +0900

    FEAT: (#27) service 분리를 진행한다

--- src/main/java/com/meetup/server/event/application/MiddlePointService.java
+++ src/main/java/com/meetup/server/event/application/MiddlePointService.java
@@ -1,13 +1,14 @@
 package com.meetup.server.event.application;
 
 import com.meetup.server.event.domain.Event;
+import com.meetup.server.event.exception.EventErrorType;
+import com.meetup.server.event.exception.EventException;
 import com.meetup.server.event.implement.EventReader;
+import com.meetup.server.event.implement.EventValidator;
 import com.meetup.server.global.util.CoordinateUtil;
 import com.meetup.server.startpoint.domain.StartPoint;
 import com.meetup.server.startpoint.implement.StartPointReader;
 import com.meetup.server.subway.domain.Subway;
-import com.meetup.server.subway.exception.SubwayErrorType;
-import com.meetup.server.subway.exception.SubwayException;
 import com.meetup.server.subway.implement.processor.SubwayPathResult;
 import com.meetup.server.subway.implement.processor.SubwayProcessor;
 import com.meetup.server.subway.implement.reader.SubwayReader;
@@ -29,15 +30,19 @@ public class MiddlePointService {
     private final StartPointReader startPointReader;
     private final SubwayProcessor subwayProcessor;
     private final SubwayReader subwayReader;
+    private final EventValidator eventValidator;
 
     public void getMiddlePoint(UUID eventId) {
         Event event = eventReader.read(eventId);
         List<StartPoint> startPoints = startPointReader.readAllByEvent(event);
+        eventValidator.validateMinimumStartPoints(startPoints);
 
         Map<StartPoint, Subway> startPointToSubwayMap = subwayProcessor.mapStartPointsToClosestSubway(startPoints);
+        eventValidator.validateStartPointsNotAllSameSubway(startPointToSubwayMap);
 
         Point centerPoint = CoordinateUtil.calculateCenterPoint(startPoints.stream().map(StartPoint::getPoint).toList());
         List<Subway> nearbySubways = subwayReader.readNearbySubways(centerPoint);
+        eventValidator.validateNearbySubwaysExist(nearbySubways);
 
         Map<StartPoint, List<SubwayPathResult>> startPointToSubwayPathsMap = subwayProcessor.mapStartPointsToDestinationSubways(startPoints, startPointToSubwayMap, nearbySubways);
 
@@ -45,7 +50,7 @@ public class MiddlePointService {
             Subway subway = subwayReader.read(subwayId);
             log.info("중간지점 Subway: {} ({})", subway.getName(), subway.getSubwayId());
         }, () -> {
-            throw new SubwayException(SubwayErrorType.SUBWAY_NOT_FOUND);
+            throw new EventException(EventErrorType.PATH_CALCULATION_FAILED);
         });
     }
 }
