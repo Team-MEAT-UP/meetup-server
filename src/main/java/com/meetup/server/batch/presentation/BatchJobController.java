097d49648b1684294eeed06f227d689f45519891
anxi01 <tjdals2893@naver.com>
2025-05-19 04:40:27 +0900

    FEAT: (#71) 배치 실행시, SecretKey를 검증한다

--- src/main/java/com/meetup/server/batch/presentation/BatchJobController.java
+++ src/main/java/com/meetup/server/batch/presentation/BatchJobController.java
@@ -1,24 +1,30 @@
 package com.meetup.server.batch.presentation;
 
 import com.meetup.server.batch.place.job.RecommendPlaceSaveJob;
+import com.meetup.server.global.support.error.GlobalErrorType;
 import com.meetup.server.global.support.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Hidden;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/batch")
 public class BatchJobController {
 
+    @Value("${batch.secret-key}")
+    private String batchSecretKey;
+
     private final RecommendPlaceSaveJob recommendPlaceSaveJob;
 
     @Hidden
     @PostMapping("/recommend-place")
-    public ApiResponse<?> runRecommendPlaceSaveJob(@RequestParam int page) {
+    public ApiResponse<?> runRecommendPlaceSaveJob(@RequestParam int page, @RequestHeader String secretKey) {
+        if (!batchSecretKey.equals(secretKey)) {
+            return ApiResponse.error(GlobalErrorType.UNAUTHORIZED);
+        }
+
         recommendPlaceSaveJob.saveRecommendPlaceJobScheduler(page);
         return ApiResponse.success();
     }
