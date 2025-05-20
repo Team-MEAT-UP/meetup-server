1ddd6b97862fba0c3eb8cee57e82abf207853397
anxi01 <tjdals2893@naver.com>
2025-05-21 06:36:07 +0900

    REFACTOR: (#73) KST 타임존을 TimeUtil에 상수로 추가한다

--- src/main/java/com/meetup/server/global/util/TimeUtil.java
+++ src/main/java/com/meetup/server/global/util/TimeUtil.java
@@ -1,9 +1,13 @@
 package com.meetup.server.global.util;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
 
 public class TimeUtil {
+
+    public static final ZoneId KST_ZONE_ID = ZoneId.of("Asia/Seoul");
+
     public static int calculateDaysAgo(LocalDateTime createdAt) {
         return (int) ChronoUnit.DAYS.between(createdAt, LocalDateTime.now());
     }
